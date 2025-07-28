0c22df37581b8a319d20e2ad6240edbc57a83613
Lo <lolovespi@gmail.com>
2025-07-28 13:15:28 -0500

    updated content

---
+++ src/content/media.ts
@@ -0,0 +1,149 @@
+export const mediaItems = [
+  {
+    title: "Vulnerability Management: Assessing the Risks with CVSS, CISA KEV, EPSS, and SSVC",
+    description:
+      "LinkedIn Learning.  Vulnerability management is a critical part of an effective information security program, but determining which vulnerabilities to address first is a daunting challenge. In this course, learn how to differentiate between vulnerability severity and vulnerability risk. Discover what elements can be combined to determine vulnerability severity, techniques you can use to determine the risk a vulnerability poses to your specific environment, and things to consider when building a vulnerability management program.",
+    date: "July 14, 2025",
+    link: "https://www.linkedin.com/learning/vulnerability-management-assessing-the-risks-with-cvss-cisa-kev-epss-and-ssvc",
+    type: "video",
+  },
+    {
+    title: "LinkedIn Learning: Vulnerability Management in Cybersecurity: The Basics",
+    description:
+      "LinkedIn Learning.  This course provides those without prior experience in the area an overview of why vulnerabilities exist, as well as an explanation of the process of managing them from start to finish. Instructor Lora Vaughn McIntosh covers the three key components of vulnerability management and the tools needed to establish a program of your own. Get an introduction to vulnerability scanning and reporting, learn how to identify which vulnerabilities to address first, discover how to vet false positives, and more.",
+    date: "July 8, 2025",
+    link: "https://www.linkedin.com/learning/vulnerability-management-in-cybersecurity-the-basics",
+    type: "video",
+  },
+   {
+    title: "Plan for Chaos: Why Most Incident Response Plans Fail Big",
+    description:
+      "ncident response plans are critical to any cybersecurity strategy, but the reality is that many of these plans aren’t enough when faced with major, high-impact incidents. Despite hours of preparation, training, and simulation exercises, when a truly significant event occurs, many organizations struggle to respond effectively. This talk will dive into the gaps and misconceptions inherent in most incident response plans, why they fail during major incidents, and how to evolve beyond them. ",
+    date: "April 10, 2025",
+    link: "",
+    type: "talk",
+  },
+    {
+    title: "Measuring Cybersecurity Success: Crafting metrics with NIST CSF and Beyond",
+    description:
+      "ISC2 Security Congress 2024: Most cybersecurity leaders have a good understanding of the things that need to be done to improve a security program, but how do you show senior leaders and board members those needs and progress as improvements are made? In this talk, the speaker will show an approach that uses the NIST Cyber Security Framework (CSF) to conduct a mat",
+    date: "October 15, 2024",
+    link: "https://events.isc2.org/p/s/measuring-cybersecurity-success-crafting-metrics-with-nist-csf-and-beyond-6467",
+    type: "talk",
+  },
+  
+  {
+    title: "Cybersecurity in Banking and Finance: 6 Examples to Know",
+    description:
+      "Article with insights from Lora Vaughn, about cybersecurity in banking.",
+    date: "March 29 2023",
+    link: "https://builtin.com/articles/cybersecurity-banking-financial-services",
+    type: "article",
+  },
+  
+  {
+    title: "Courageous Kat",
+    description:
+      "A tribute to Kathy 'Kat' Vaughn, Lora's mother, who was a pioneer for women in the workforce and a source of inspiration.",
+    date: "March 8 2023",
+    link: "https://www.linkedin.com/pulse/courageous-kat-lora-vaughn/",
+    type: "article",
+  },   
+   {
+    title: "A Bug (Bounty) Story",
+    description: "Presentation at 2022 Southeast Cybersecurity Summit about building effective bug bounty programs",
+    date: "April , 2022",
+    link: "",
+    type: "talk",
+  },
+  {
+    title: "A Guide to Cybersecurity Careers",
+    description:
+      "Transitioning from a cybersecurity student to a cybersecurity professional sounds daunting, but it doesn’t have to be.",
+    date: "January 25, 2022",
+    link: "https://www.youtube.com/watch?v=n7-gHo74Ozc&ab_channel=TechBirmingham",
+    type: "video",
+  }, 
+   {
+    title: "The Myth of the IT and Cybersecurity Meritocracy",
+    description:
+      "Article in Business and Tech.  The idea that anyone can become a successful IT or cybersecurity professional through hard work and talent is a myth. In reality, systemic barriers and biases often prevent many qualified individuals from entering or advancing in these fields.",
+    date: "January 25, 2022",
+    link: "https://www.futureofbusinessandtech.com/digital-security/the-myth-of-the-it-and-cybersecurity-meritocracy/",
+    type: "article",
+  }, 
+  {
+    title: "Empathy: Your Career Superpower",
+    description: "Presentation at 2021 Cyber Now Summit",
+    date: "April 27, 2021",
+    link: "",
+    type: "talk",
+  },
+  {
+    title: "A CISO's Guide to Cybersecurity Careers",
+    description:
+      "Transitioning from a cybersecurity student to a cybersecurity professional sounds daunting, but it doesn’t have to be.",
+    date: "November 11, 2020",
+    link: "https://www.wicys.org/wp-content/uploads/2020/11/2020_WiCyS_Program_FINAL.pdf",
+    type: "talk",
+  },  
+  {
+    title: "Flip This House: Overhauling a cyber program and culture with NIST CSF ",
+    description:
+      "Presentation at 2020  Cyber Now Summit",
+    date: "September 2, 2020",
+    link: "",
+    type: "talk",
+  },
+   {
+    title: "5 Potential Red Flags in Job Postings",
+    description:
+      "What are some of the red flags to look for when applying for a job in cybersecurity? Lora Vaughn shares her insights.",
+    date: "July 13, 2020",
+    link: "  https://www.linkedin.com/pulse/5-potential-red-flags-job-postings-lora-vaughn/",
+    type: "article",
+  }, 
+  {
+    title: "How Developers and Cybersecurity Teams Can Improve Communication",
+    description:
+      "Article in Built In.  Lora Vaughn and other cybersecurity leaders discuss how developers and cybersecurity teams can work together more effectively with Tammy Xu.",
+    date: "May 6, 2020",
+    link: "https://builtin.com/software-engineering-perspectives/collaboration-between-developers-security",
+    type: "article",
+  },
+  
+  {
+    title: "Simmons Bank talks about the best security practices to keep your information safe",
+    description:
+      "KATV Little Rock, Lora Vaughn discusses cybersecurity best practices.",
+    date: "August 31, 2019",
+    link: "https://share.vidyard.com/watch/c5ee7ifttbSnjXUMEnoy6C?custom_id=",
+    type: "video",
+  },
+  {
+    title: "Meet the Female Leaders Of Finance: Lora Vaughn",
+    description:
+      "Lora Vaughn of Simmons Bank: “When we collaborate and engage outside our bubbles, we can come up with stronger solutions.”",
+    date: "July 1, 2019",
+    link: "https://medium.com/authority-magazine/meet-the-female-leaders-of-finance-when-we-collaborate-and-engage-outside-our-bubbles-we-can-cca62c36402d",
+    type: "article",
+  },
+  
+  {
+    title: "You Can't Boil the Ocean: Building a Risk Based Vulnerability Management Program",
+    description:
+      "Presentation at 2018 Alabama Cyber Now Conference",
+    date: "April 11, 2018",
+    link: "",
+    type: "talk",
+  },
+  {
+    title: "Keepin' It Real: Idenftifying 'Real Risk' and Building a Threat-Centric Vulnerability Management Program",
+    description:
+      "Rapid7 UNITED2017 presentation",
+    date: "September 13, 2017",
+    link: "https://share.vidyard.com/watch/c5ee7ifttbSnjXUMEnoy6C?custom_id=",
+    type: "video",
+  },
+  
+];
